d5585d90086da99350cbc9f04175c576dfb17e3d
alex-kay <ichijoint@gmail.com>
2021-09-23 12:03:13 +0000

    added template

--- main.tf
+++ main.tf
@@ -10,3 +10,22 @@ resource "google_project_service" "compute_engine" {
   disable_dependent_services = false
   disable_on_destroy         = false
 }
+resource "null_resource" "example0" {
+    provisioner "local-exec" {
+        command = "rm hosts"
+  }
+}
+resource "null_resource" "example1" {
+  provisioner "local-exec" {
+    command = "echo 'vm1 ${google_compute_instance.vm1.network_interface.0.access_config.0.nat_ip}' >> hosts"
+    
+  }
+  depends_on = [null_resource.example0, google_compute_instance.vm1]
+}
+resource "null_resource" "example2" {
+  provisioner "local-exec" {
+    command = "echo 'vm1 ${google_compute_instance.vm2.network_interface.0.access_config.0.nat_ip}' >> hosts"
+    
+  }
+  depends_on = [null_resource.example0, google_compute_instance.vm2]
+}
